1cc481c4889df8e34885f304a452cc83f61dcf5a
yuki <yuki.laurentiu@gmail.com>
2023-05-17 11:30:09 -0600

    add Theme and separate stateless class

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'input_page.dart';
+
 
 void main() => runApp(const BMICalculator());
 
@@ -7,31 +9,15 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
-      home: InputPage(),
-    );
-  }
-}
+      theme: ThemeData.dark().copyWith(
+        colorScheme: ThemeData().colorScheme.copyWith(
+          primary: const Color(0xFF0A0E21),
 
-class InputPage extends StatefulWidget {
-  const InputPage({Key? key}) : super(key: key);
-  @override
-  _InputPageState createState() => _InputPageState();
-}
+        ),
+        scaffoldBackgroundColor: const Color(0xFF0A0E21),
+      ),
 
-class _InputPageState extends State<InputPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI CALCULATOR'),
-      ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      // floatingActionButton: FloatingActionButton(
-      //   child: Icon(Icons.add),
-      // ),
+    home: InputPage(),
     );
   }
 }
